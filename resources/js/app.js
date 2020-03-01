133b787ef8425a09f352ae2d043b34d64b974a07
aqodad <aqodad.ouahid26@gmail.com>
2020-03-01 14:54:30 +0100

    create vue router with home route

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import routes from ';/routes';
+const router = new VueRouter({
+    routes
+});
+
+new Vue({
+    router,
+    render: h => h(App)
+}).mount('#app');
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('auth')->group(function () {
+    Route::get('init', 'AppController@init');
+
+    Route::post('login', 'AppController@login');
+    Route::post('register', 'AppController@register');
+    Route::post('logout', 'AppController@logout');
+});
